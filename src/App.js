ccccdba82a95434cd8233885ffa3b1755517baf2
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2024-07-01 16:18:32 +0300

    init project structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+
+const App = () => {
+	return (
+		<main className='page'>
+			<Header />
+			<Hero />
+		</main>
+	);
+};
+
+export default App;
